7994546629d2eb216355cc78e375d6ed83278dc2
Marcio <marcio.de.carvalho@tecnico.ulisboa.pt>
2023-12-28 15:33:00 +0000

    added some function
    just a functin that returns the elements with a length higher than 5

--- some.py
+++ some.py
@@ -5,3 +5,10 @@ def func(argument):
             if len(c) > 5:
                 lista.append(c)
     return lista
+
+def getchar(palavra):
+    l = []
+    for i in palavra:
+        if 'h' in i:
+            l.append(i)
+    return l
